e07755a4339d805c699d7714b6feaca035fb6fed
Yana Nazarenko <dnd1993@yandex.ru>
2021-07-29 09:07:21 -0400

    updated files

---
+++ src/useReducer/grocery_list/reducer.js
@@ -0,0 +1,53 @@
+export const reducer = (state, action) => {
+  if (action.type === "LIST_ADD") {
+    const newList = [...state.list, action.payload.newItem];
+    return {
+      ...state,
+      showModal: true,
+      modalMessage: "Item was added",
+      list: newList,
+    };
+  }
+  if (action.type === "NO_ITEM") {
+    return { ...state, showModal: true, modalMessage: "Please enter value" };
+  }
+  if (action.type === "CLOSE_MODAL") {
+    return { ...state, showModal: false };
+  }
+  if (action.type === "EDIT_ITEM") {
+    const currentId = action.payload;
+    return { ...state, isEditing: true, currentId };
+  }
+  if (action.type === "LIST_UPDATE") {
+    const currItemIndex = state.list.findIndex(
+      (item) => item.id === state.currentId
+    );
+    state.list[currItemIndex].item = action.payload.item;
+    action.payload.setItem("");
+    return {
+      ...state,
+      showModal: true,
+      modalMessage: "Item has been edited",
+      isEditing: false,
+    };
+  }
+  if (action.type === "DELETE_ITEM") {
+    const newList = state.list.filter((item) => item.id !== action.payload);
+    return {
+      ...state,
+      showModal: true,
+      modalMessage: "Item deleted",
+      list: newList,
+    };
+  }
+  if (action.type === "ERASE_ITEMS") {
+    const newList = [];
+    return {
+      ...state,
+      showModal: true,
+      modalMessage: "List is empty",
+      list: newList,
+    };
+  }
+  throw new Error("There is no such action type");
+};
